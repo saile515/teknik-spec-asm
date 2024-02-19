91f9b675aadfecc48ae8a59ce296126e3fd3a4a6
saile515 <elias.jorgensen2006@gmail.com>
2024-02-19 09:37:04 +0000

    fix indentation

--- gameoflife.asm
+++ gameoflife.asm
@@ -6,8 +6,8 @@ extern malloc, free
 
 section .data
 
-width: dq 40
-height: dq 20
+width: dq 80
+height: dq 40
 ; cells that are filled in the beginning of the simulation, format: [x1, y1, x2, y2, ...] (0 indexed)
 initial_cells: db 7, 5, 6, 6, 7, 6, 8, 6
 initial_cells_length: dq 8
@@ -41,17 +41,17 @@ main:
 
     mov r8, 0                               ; loop variable
     .init_cells:
-        mov rdi, 0                          ; clear parameter registers
-        mov rsi, 0
-        mov dil, [initial_cells+r8]          ; x
-        mov sil, [initial_cells+r8+1]        ; y
-        call coordinates_to_index
-        mov rbx, [cell_map]
-        add rbx, rax
-        mov byte [rbx], 1          ; turn on cell
-        add r8, 2                           ; increment once for x and once for y
-        cmp r8, [initial_cells_length]
-        jne .init_cells
+    mov rdi, 0                          ; clear parameter registers
+    mov rsi, 0
+    mov dil, [initial_cells+r8]          ; x
+    mov sil, [initial_cells+r8+1]        ; y
+    call coordinates_to_index
+    mov rbx, [cell_map]
+    add rbx, rax
+    mov byte [rbx], 1          ; turn on cell
+    add r8, 2                           ; increment once for x and once for y
+    cmp r8, [initial_cells_length]
+    jne .init_cells
 
     call print_frame
     
@@ -74,73 +74,73 @@ evaluate_next_frame:
     mov qword [cell_map], rax
 
     .evaluate_cell:
-        sub rsp, 24                         ; allocate local variables
-        mov qword [rbp-16], 0               ; x
-        mov qword [rbp-24], 0               ; y
-        mov qword [rbp-32], 0               ; neighbouring cells on count
-        mov rdi, [rbp-8]                    ; get coordinates
-        call index_to_coordinates
-        mov dword [rbp-24], eax
-        shr rax, 32
-        mov dword [rbp-16], eax
-        mov r8, -1                          ; loop variable x
-        mov r9, -1                          ; loop variable y
-        .for_x:
-        .for_y:
-            cmp r8, 0           ; check if cell offset is 0, 0
-            jne .start_increment
-            cmp r9, 0
-            jne .start_increment
-            jmp .end_increment
-            .start_increment:
-            mov rdi, [rbp-16]               ; get cell value of neighbouring cells
-            add rdi, r8
-            mov rsi, [rbp-24]
-            add rsi, r9
-            call get_cell_value
-            add [rbp-32], rax
-            .end_increment:
-            inc r9
-            cmp r9, 1
-            jbe .for_y
-            jmp .end_y
-            .end_y:
-                mov r9, -1
-                inc r8
-                cmp r8, 1
-                jbe .for_x
-                jmp .end_x
-        .end_x:
-        mov rax, [rbp-8]
-        add rax, [cell_map]
-        push rax
-        mov rbx, [rbp-32]
-        cmp qword [rbp-32], 2
-        jb .off
-        cmp qword [rbp-32], 3
-        ja .off
-        je .on
-        mov rdi, [rbp-16]
-        mov rsi, [rbp-24]
-        call get_cell_value
-        cmp rax, 1
-        jb .off
-        jmp .on
-        .on:
-        pop rax
-        mov byte [rax], 1
-        jmp .end_evaluate_cell
-        .off:
-        pop rax
-        mov byte [rax], 0
-        .end_evaluate_cell:
-        add rsp, 24
-        mov rax, [rbp-8]
-        inc rax
-        mov qword [rbp-8], rax
-        mov rax, [map_size]
-        cmp qword [rbp-8], rax
-        jb .evaluate_cell
+    sub rsp, 24                         ; allocate local variables
+    mov qword [rbp-16], 0               ; x
+    mov qword [rbp-24], 0               ; y
+    mov qword [rbp-32], 0               ; neighbouring cells on count
+    mov rdi, [rbp-8]                    ; get coordinates
+    call index_to_coordinates
+    mov dword [rbp-24], eax
+    shr rax, 32
+    mov dword [rbp-16], eax
+    mov r8, -1                          ; loop variable x
+    mov r9, -1                          ; loop variable y
+    .for_x:
+    .for_y:
+    cmp r8, 0           ; check if cell offset is 0, 0
+    jne .start_increment
+    cmp r9, 0
+    jne .start_increment
+    jmp .end_increment
+    .start_increment:
+    mov rdi, [rbp-16]               ; get cell value of neighbouring cells
+    add rdi, r8
+    mov rsi, [rbp-24]
+    add rsi, r9
+    call get_cell_value
+    add [rbp-32], rax
+    .end_increment:
+    inc r9
+    cmp r9, 1
+    jbe .for_y
+    jmp .end_y
+    .end_y:
+    mov r9, -1
+    inc r8
+    cmp r8, 1
+    jbe .for_x
+    jmp .end_x
+    .end_x:
+    mov rax, [rbp-8]
+    add rax, [cell_map]
+    push rax
+    mov rbx, [rbp-32]
+    cmp qword [rbp-32], 2
+    jb .off
+    cmp qword [rbp-32], 3
+    ja .off
+    je .on
+    mov rdi, [rbp-16]
+    mov rsi, [rbp-24]
+    call get_cell_value
+    cmp rax, 1
+    jb .off
+    jmp .on
+    .on:
+    pop rax
+    mov byte [rax], 1
+    jmp .end_evaluate_cell
+    .off:
+    pop rax
+    mov byte [rax], 0
+    .end_evaluate_cell:
+    add rsp, 24
+    mov rax, [rbp-8]
+    inc rax
+    mov qword [rbp-8], rax
+    mov rax, [map_size]
+    cmp qword [rbp-8], rax
+    jb .evaluate_cell
     call print_frame
 
     add rsp, 8
@@ -160,24 +160,24 @@ print_frame:
     call print_string
     mov r8, 0
     .print_y:
-        mov r9, 0
-        .print_x:
-            mov rdi, r9
-            mov rsi, r8
-            call coordinates_to_index
-            mov rdi, 0
-            add rax, [cell_map]
-            mov dil, [rax]
-            add rdi, 32
-            call print_char
-            inc r9
-            cmp r9, [width]
-            jb .print_x
-        mov rdi, 10
-        call print_char
-        inc r8
-        cmp r8, [height]
-        jne .print_y
+    mov r9, 0
+    .print_x:
+    mov rdi, r9
+    mov rsi, r8
+    call coordinates_to_index
+    mov rdi, 0
+    add rax, [cell_map]
+    mov dil, [rax]
+    add rdi, 32
+    call print_char
+    inc r9
+    cmp r9, [width]
+    jb .print_x
+    mov rdi, 10
+    call print_char
+    inc r8
+    cmp r8, [height]
+    jne .print_y
     ret
 
 ; int coordinates_to_index(int x, int y)
@@ -240,8 +240,8 @@ get_cell_value:
     add rax, [cell_map_backbuffer]
     cmp byte [rax], 1
     jb .end
-mov rax, 1
-ret
-.end:
-mov rax, 0
-ret
+    mov rax, 1
+    ret
+    .end:
+    mov rax, 0
+    ret
